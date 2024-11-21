59454b22187d06562df77116c79e5e315fee8e22
Rohitjakkam <rohitjakkam@gmail.com>
2024-11-22 02:37:15 +0530

    remove key

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Gemini API Configuration
-GEMINI_API_KEY = "AIzaSyA2vJPfQpe1rqoEy0iv0N_w0tehujAVVkY"  # Replace with your actual API key
+GEMINI_API_KEY = ""  # Replace with your actual API key
 GEMINI_BASE_URL = "https://generativelanguage.googleapis.com"  # Replace with the actual base URL
 
 # Other settings can go here
